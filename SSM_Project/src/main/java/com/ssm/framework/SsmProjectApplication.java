0887fea12a64b14232f312c2e2d04b7c86e8e5f5
RothschildsMa <unityidma@gmail.com>
2023-08-08 16:55:13 +0900

    あ

--- SSM_Project/src/main/java/com/ssm/framework/SsmProjectApplication.java
+++ SSM_Project/src/main/java/com/ssm/framework/SsmProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SsmProjectApplication {
 
+	//こんにちは
 	public static void main(String[] args) {
 		SpringApplication.run(SsmProjectApplication.class, args);
 	}
