74e87d5c8bbd71ef2312fa257320b23f9cabefd0
RothschildsMa <unityidma@gmail.com>
2023-08-08 13:44:58 +0900

    first commit

---
+++ SSM_Project/src/main/java/com/ssm/framework/SsmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ssm.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmProjectApplication.class, args);
+	}
+
+}
---
+++ SSM_Project/src/test/java/com/ssm/framework/SsmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssm.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
